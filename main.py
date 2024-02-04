68c6aee321d3d9ce2a21fcd88b3a66c992540421
beavargas <79265445+beavargas@users.noreply.github.com>
2024-02-04 14:31:28 +0000

    loading json config file into main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Main file 
+import json
+
+# The following function loads the config.json file
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def main():
+    config = load_config('config.json')
+
+
+    return
+
+if __name__ == '__main__':
+    main()
