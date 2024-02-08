db1e283c5367054af77fb52129fcae42d40d2afd
beavargas <79265445+beavargas@users.noreply.github.com>
2024-02-08 19:31:33 +0000

    removing errors an dmakeing the code reusable

--- Festival.py
+++ Festival.py
@@ -15,29 +15,31 @@ class Festival(object):
         self.total_festival_goers = total_festival_goers
     
     def ticket_scan(self):
-        scanning_time = np.random.normal(self.mean_scan_time, self.std_scan_time)
+        scanning_time = max(0, np.random.normal(self.mean_scan_time, self.std_scan_time))
         yield self.env.timeout(scanning_time)
 
     def get_waiting_times_per_server(self):
+        #return(self.get_waiting_times_per_server)
         return np.array(self.waiting_times_per_server)
 
-def go_to_festival(env, festival_goer, festival):
+def go_to_festival(env, festival):
 
     arrival_time = env.now # Record the current simulation time
 
     with festival.server.request() as request:
         yield request
-        yield env.process(festival.ticket_scan(festival_goer))
+        yield env.process(festival.ticket_scan())
     
     festival_goer_waiting_time = env.now - arrival_time
+    festival.waiting_times_per_server.append(festival_goer_waiting_time)
     return festival_goer_waiting_time
 
 def run_festival(env, servers, mean_interarrival, std_interarrival, total_festival_goers, festival):
-    festival_goer = 0
+    festival_goer = 1
     while festival_goer <= total_festival_goers:
         interarrival_time = max(0, random.normalvariate(mean_interarrival, std_interarrival))
         yield env.timeout(interarrival_time)
-        env.process(go_to_festival(env, festival_goer, festival))
+        env.process(go_to_festival(env, festival))
         festival_goer += 1
 
         
