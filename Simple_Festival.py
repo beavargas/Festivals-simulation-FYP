7c32ce66878514ad2b41f5904b04d3d5259783ce
beavargas <79265445+beavargas@users.noreply.github.com>
2024-03-03 20:06:20 +0100

    create file for simple model

---
+++ Simple_Festival.py
@@ -0,0 +1,47 @@
+# Festival class and processes
+import simpy
+import random
+import numpy as np
+
+
+class Festival(object):
+
+    def __init__(self, env, servers, mean_scan_time, std_scan_time, waiting_times_per_server,  total_festival_goers):
+        self.env = env
+        self.server = simpy.Resource(env, capacity=servers)
+        self.mean_scan_time = mean_scan_time
+        self.std_scan_time = std_scan_time
+        self.waiting_times_per_server = []
+        self.total_festival_goers = total_festival_goers
+
+    def ticket_scan(self):
+        yield self.env.timeout(random.randint(1,3)) 
+        
+
+    def get_waiting_times_per_server(self):
+        #return(self.get_waiting_times_per_server)
+        return np.array(self.waiting_times_per_server)
+
+def go_to_festival(env, festival):
+
+    arrival_time = env.now # Record the current simulation time
+
+    with festival.server.request() as request:
+        yield request
+        yield env.process(festival.ticket_scan())
+    
+    festival_goer_waiting_time = env.now - arrival_time
+    festival.waiting_times_per_server.append(festival_goer_waiting_time)
+    return festival_goer_waiting_time
+
+def run_festival(env, servers, mean_interarrival, std_interarrival, total_festival_goers, festival):
+    festival_goer = 1
+    while festival_goer <= total_festival_goers:
+        interarrival_time = 0.20
+        yield env.timeout(interarrival_time)
+        env.process(go_to_festival(env, festival))
+        festival_goer += 1
+
+        
+
+
