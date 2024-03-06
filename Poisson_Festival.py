2bf112b3a41f057fab0bb7abb1cc2361cb2cde91
beavargas <79265445+beavargas@users.noreply.github.com>
2024-03-06 10:41:39 +0000

    adding security check function

--- Poisson_Festival.py
+++ Poisson_Festival.py
@@ -6,14 +6,20 @@ import numpy as np
 
 class Festival(object):
 
-    def __init__(self, env, servers, mean_scan_time, std_scan_time, waiting_times_per_server,  total_festival_goers):
+    def __init__(self, env, servers, mean_security_time, std_security_time, mean_scan_time, std_scan_time, waiting_times_per_server,  total_festival_goers):
         self.env = env
         self.server = simpy.Resource(env, capacity=servers)
-        #self.lamda_scan_time = lamda_scan_time
+        
+        self.mean_security_time = mean_security_time
+        self.std_security_time = std_security_time
         self.mean_scan_time = mean_scan_time
         self.std_scan_time = std_scan_time
         self.waiting_times_per_server = []
         self.total_festival_goers = total_festival_goers
+    
+    def security_check(self):
+        security_time = max(0, np.random.normal(self.mean_security_time, self.std_security_time))
+        yield self.env.timeout(security_time)
     
     def ticket_scan(self):
         #scanning_time = max(0, np.random.poisson(self.lamda_scan_time))
@@ -27,6 +33,9 @@ class Festival(object):
 def go_to_festival(env, festival):
 
     arrival_time = env.now # Record the current simulation time
+
+    # Perform security check
+    yield env.process(festival.security_check())
 
     with festival.server.request() as request:
         yield request
