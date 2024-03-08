40b3cf319e28353417f1dd929daae9f0acd3a36d
beavargas <79265445+beavargas@users.noreply.github.com>
2024-03-08 13:21:03 +0000

    add groups

--- Simple_Festival.py
+++ Simple_Festival.py
@@ -6,11 +6,9 @@ import numpy as np
 
 class Festival(object):
 
-    def __init__(self, env, servers, mean_scan_time, std_scan_time, waiting_times_per_server,  total_festival_goers):
+    def __init__(self, env, servers, waiting_times_per_server,  total_festival_goers):
         self.env = env
         self.server = simpy.Resource(env, capacity=servers)
-        self.mean_scan_time = mean_scan_time
-        self.std_scan_time = std_scan_time
         self.waiting_times_per_server = []
         self.total_festival_goers = total_festival_goers
 
@@ -34,13 +32,35 @@ def go_to_festival(env, festival):
     festival.waiting_times_per_server.append(festival_goer_waiting_time)
     return festival_goer_waiting_time
 
-def run_festival(env, servers, mean_interarrival, std_interarrival, total_festival_goers, festival):
-    festival_goer = 1
+def run_festival(env, servers, total_festival_goers, festival):
+    #festival_goer = 1
+    #while festival_goer <= total_festival_goers:
+        
+    #    yield env.timeout(interarrival_time)
+    #    env.process(go_to_festival(env, festival))
+    #    festival_goer += 1
+    
+    festival_goer = 0
     while festival_goer <= total_festival_goers:
         interarrival_time = 0.20
-        yield env.timeout(interarrival_time)
-        env.process(go_to_festival(env, festival))
-        festival_goer += 1
+
+        group_size = random.randint(1,8)
+
+        remaining_people = total_festival_goers - festival_goer
+
+        group_size = min(group_size, remaining_people)
+    
+        for person in range(group_size):
+                yield env.timeout(interarrival_time)
+                env.process(go_to_festival(env, festival))
+                print(interarrival_time)
+            
+        festival_goer += group_size
+        print(festival_goer)
+
+        if festival_goer >= total_festival_goers:
+            break
+
 
         
 
