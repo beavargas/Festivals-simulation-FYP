8d2e38b6c02b6f7a0c8f583d853c925b9488e77f
beavargas <79265445+beavargas@users.noreply.github.com>
2024-03-22 19:04:34 +0000

    fix

--- run_experiment.py
+++ run_experiment.py
@@ -1,3 +1,4 @@
+
 import json
 from main import main
 import numpy as np
@@ -6,8 +7,12 @@ import pandas as pd
 def run_experiment(num_runs):
     mean_waiting_times = []
     std_waiting_times = []
-
+    simulation_sample_means = []
+    simulation_sample_std = []
+    
+    
     for i in range(num_runs):
+        print(i)
         main()
         simulation_data_df = pd.read_json('simulation_data.json')
         #print(simulation_data_df)
@@ -23,27 +28,38 @@ def run_experiment(num_runs):
         std_waiting_times.append(run_waiting_times_std)
     
     df_mean_waiting_times = pd.DataFrame(mean_waiting_times).T
-    print(df_mean_waiting_times)
+    num_rows, num_cols = df_mean_waiting_times.shape
+
+    print("Number of rows:", num_rows)
+    print("Number of columns:", num_cols)
+
+
     df_std_waiting_times = pd.DataFrame(std_waiting_times).T
-    print(df_std_waiting_times)
-             
+    #print(df_std_waiting_times)
 
-        #with open(f'simulation_data.json', 'r') as file:
-            #simulation_data = json.load(file)
-            #print(simulation_data)
-            #simulation_data_df = pd.DataFrame(simulation_data)
-            #print(simulation_data_df.head())
+    a = df_mean_waiting_times.to_json('simulation_sample_means.json')
+ 
+    
+    #for index, row in df_mean_waiting_times.iterrows():
+        #sample_mean = row.mean()
+        #sample_std = row.std()
+        #simulation_sample_means.append(sample_mean)
+        #simulation_sample_std.append(sample_std)
+    
+    
+    
+    #with open('simulation_sample_means.json', 'w') as file:
+        #json.dump(simulation_sample_means, file, indent=1)
+    
+    #with open('simulation_sample_std.json', 'w') as file:
+        #json.dump(simulation_sample_std, file, indent=1)
+
 
             
-    
-   
-    
-    
-            
-            
+        
             
 if __name__ == '__main__':
-    run_experiment(5)
+    run_experiment(50)
     
 
 
