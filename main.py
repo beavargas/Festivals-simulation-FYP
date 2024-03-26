bf114590f26ef78aa644f04b3ed78c547f1de51c
beavargas <79265445+beavargas@users.noreply.github.com>
2024-03-26 19:10:02 +0100

    change random seed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Main file 
 import json
+import time
 #import pandas as pd
 import random
 import simpy
 import numpy as np
-#from Simple_Festival import Festival, go_to_festival, run_festival
-from Normal_Festival import Festival, go_to_festival, run_festival
+from Simple_Festival import Festival, go_to_festival, run_festival
+#from Normal_Festival import Festival, go_to_festival, run_festival
 #from Poisson_Festival import Festival, go_to_festival, run_festival
 #from Multimodal_Festival import Festival, go_to_festival, run_festival
 
@@ -27,19 +28,20 @@ def main():
         #print("new servers round")
         server_i_data = []
 
-        random.seed(42)
-
+        #random.seed(42)
+        np.random.seed(int(time.time()))
+        
         # Creating a simulation enviroment
         env = simpy.Environment()
 
         # Creating a Festival instance
-        #festival = Festival(env, servers, server_i_data, config["total_festival_goers"])
+        festival = Festival(env, servers, server_i_data, config["total_festival_goers"])
         #festival = Festival(env, servers, config["mean_security_time"], config["std_security_time"], config["mean_scan_time"], config["std_scan_time"], server_i_data, config["total_festival_goers"])
-        festival = Festival(env, servers, config["mean_security_time"], config["std_security_time"], config["mean_scan_time"], config["std_scan_time"], server_i_data, config["total_festival_goers"])        
+        #festival = Festival(env, servers, config["mean_security_time_short"], config["std_security_time_short"],config["mean_security_time_long"], config["std_security_time_long"], config["mean_scan_time"], config["std_scan_time"], server_i_data, config["total_festival_goers"])        
         
         # Running the festival simulation process
-        #env.process(run_festival(env, servers, config["total_festival_goers"], festival))
-        env.process(run_festival(env, servers, config['mean_interarrival'], config['std_interarrival'],  config["total_festival_goers"], config["mean_group_size"], config["std_group_size"], festival))
+        env.process(run_festival(env, servers, config["total_festival_goers"], festival))
+        #env.process(run_festival(env, servers, config['mean_interarrival'], config['std_interarrival'],  config["total_festival_goers"], config["mean_group_size"], config["std_group_size"], festival))
         #env.process(run_festival(env, servers, config["lamda_interarrival"], config["total_festival_goers"], config["mean_group_size"], config["std_group_size"], festival))
         
         # Running the simulation until a specified duration
