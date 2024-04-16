a4e812e660a0c5c93f33e1c70c524776f87cac84
beavargas <79265445+beavargas@users.noreply.github.com>
2024-04-16 22:59:33 +0100

    MMA model and testing

--- run_experiment.py
+++ run_experiment.py
@@ -1,6 +1,7 @@
 
 import json
-from main import main
+#from main import main
+import case_study_actual_main
 import numpy as np
 import pandas as pd
 import random
@@ -14,8 +15,8 @@ def run_experiment(num_runs):
     
     for i in range(num_runs):
         print(i)
-        main()
-        simulation_data_df = pd.read_json('simulation_data.json')
+        case_study_actual_main.main()
+        simulation_data_df = pd.read_json('case_study.json')
         #print(simulation_data_df)
         run_waiting_times_mean = []
         run_waiting_times_std = []
@@ -31,14 +32,14 @@ def run_experiment(num_runs):
     df_mean_waiting_times = pd.DataFrame(mean_waiting_times).T
     num_rows, num_cols = df_mean_waiting_times.shape
 
-    print("Number of rows:", num_rows)
-    print("Number of columns:", num_cols)
+    #print("Number of rows:", num_rows)
+    #print("Number of columns:", num_cols)
 
 
     df_std_waiting_times = pd.DataFrame(std_waiting_times).T
     #print(df_std_waiting_times)
 
-    a = df_mean_waiting_times.to_json('RESULTS_Multimodal_Model.json')
+    a = df_mean_waiting_times.to_json('RESULTS_case_study.json')
  
 
 
